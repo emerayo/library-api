80637531a009bb291cda743c7a8e51b4283a3eef
Emanuel Merayo <emanuelmerayo@hotmail.com>
2024-06-25 13:27:44 -0300

    Create current_user endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  get '/current_user', to: 'current_user#index'
+
   devise_for :users, path: '', path_names: {
                                  sign_in: 'login',
                                  sign_out: 'logout',
