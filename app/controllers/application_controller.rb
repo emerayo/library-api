66702121b78f771a3963a4d42b2b70b203c5f46c
Emanuel Merayo <emanuelmerayo@hotmail.com>
2024-06-25 16:35:58 -0300

    Add authorization to BooksController endpoints

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,11 +4,16 @@ class ApplicationController < ActionController::API
   before_action :set_response_format
 
   rescue_from ActiveRecord::RecordNotFound, with: :not_found
+  rescue_from CanCan::AccessDenied, with: :unauthorized
 
   private
 
   def not_found
     render json: { error: 'not-found' }, status: :not_found
+  end
+
+  def unauthorized
+    render json: { error: 'not authorized to perform this action' }, status: :unauthorized
   end
 
   def set_response_format
