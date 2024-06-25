9491d2080f31bdda7ab4e5a16a62b77e17bd6921
Emanuel Merayo <emanuelmerayo@hotmail.com>
2024-06-25 16:35:27 -0300

    Add cancancan, ability and spec

---
+++ app/models/ability.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+class Ability
+  include CanCan::Ability
+
+  def initialize(user)
+    if user.librarian?
+      can :manage, :all
+    elsif user.member?
+      can :read, Book
+      can :search, Book
+    end
+  end
+end
