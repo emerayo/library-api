082ee7103512bc9c77de6ab32a6dc863ea78e74b
Emanuel Merayo <emanuelmerayo@hotmail.com>
2024-06-25 15:34:00 -0300

    Add spec for CurrentUserController
    Add helper for Devise login

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require 'simplecov'
+require 'support/api_helper'
 SimpleCov.start
 
 # This file is copied to spec/ when you run 'rails generate rspec:install'
@@ -68,6 +69,7 @@ RSpec.configure do |config|
   # config.filter_gems_from_backtrace("gem name")
 
   config.include FactoryBot::Syntax::Methods
+  config.include Devise::Test::IntegrationHelpers, type: :request
 end
 
 Shoulda::Matchers.configure do |config|
